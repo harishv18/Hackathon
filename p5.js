dbc7b1981d0eef0666a622f509509f55ac08f832
Harish V <harish00411@gmail.com>
2023-12-02 13:04:21 +0530

    Hackathon Programs

--- p5.js
+++ p5.js
@@ -1,24 +1,49 @@
-let nums = [1,2,3]
 
-let output = []
-
-let a = nums[0]
-let b = nums[1]
-let c = nums[2]
-
-total = nums.length * 2
-
-for(let i=0;i<total/2;i++){
-  temp = a
-  a = b
-  b = c
-  c = temp
-  output.push([a,b,c])
-  temp = a
-  a = b
-  b = c
-  c = temp
-  output.push([c,b,a])
+function len_of_two() {
+  let nums = [0,1]
+  let output = []
+  let a = nums[0]
+  let b = nums[1]
+  total = nums.length * 2
+  for(let i=0;i<nums.length/2;i++){
+    temp = a;
+    a = b;
+    b = temp;
+    output.push([b,a]);
+    temp = a;
+    a = b;
+    b = temp;
+    output.push([b, a]);
+  }
+  return output
 }
 
-console.log(output)
+function len_of_three() {
+  let nums = [1, 2, 3];
+
+  let output = [];
+
+  let a = nums[0];
+  let b = nums[1];
+  let c = nums[2];
+
+  total = nums.length * 2;
+
+  for (let i = 0; i < total / 2; i++) {
+    temp = a;
+    a = b;
+    b = c;
+    c = temp;
+    output.push([a, b, c]);
+    temp = a;
+    a = b;
+    b = c;
+    c = temp;
+    output.push([c, b, a]);
+  }
+
+  console.log(output);
+}
+
+// len_of_three();
+console.log(len_of_two())
